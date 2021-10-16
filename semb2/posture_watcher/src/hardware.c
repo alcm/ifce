4d37c03c9a9d447fd00c70815b11064ffc438835
André L. C. Moreira <alcm99@gmail.com>
2021-10-15 23:52:43 -0300

    [semb2] refactoring makefile

--- semb2/posture_watcher/src/hardware.c
+++ semb2/posture_watcher/src/hardware.c
@@ -1,14 +1,13 @@
 #include "adxl345.h"
 #include "hardware.h"
-#include "i2c.h"
 #include "uart_logger.h"
-  
+
 void sys_config(void)
 {
-  DDRD = (1 << VIBRACALL_PIN); /* VIBRACALL_PIN as output */
-  DDRD = (1 << LED_PIN);
-  PORTD = 0x00;
+    DDRD = (1 << VIBRACALL_PIN); /* VIBRACALL_PIN as output */
+    DDRD = (1 << LED_PIN);
+    PORTD = 0x00;
 
-  setup_uart_logger(9600);
-  setup_adxl345();
+    setup_uart_logger(9600);
+    setup_adxl345();
 }
--- semb2/posture_watcher/src/posture_watcher.c
+++ semb2/posture_watcher/src/posture_watcher.c
@@ -6,54 +6,52 @@
 #include "posture_watcher.h"
 #include "uart_logger.h"
 
-#define DEBUG   1
-
-bool posture_is_correct(const float * angles)
+bool posture_is_correct(const float *angles)
 {
-  const float ideal_xyz[3] = {175.0, 88.0, 88.0}; // TODO: check these values...
+    const float ideal_xyz[3] = {175.0, 88.0, 88.0}; // TODO: check these values...
 
-  return !((fabsf(angles[0] - ideal_xyz[0]) >= 15.0) ||
-           (fabsf(angles[1] - ideal_xyz[1]) >= 15.0) ||
-           (fabsf(angles[2] - ideal_xyz[2]) >= 10.0));
+    return !((fabsf(angles[0] - ideal_xyz[0]) >= 15.0) ||
+             (fabsf(angles[1] - ideal_xyz[1]) >= 15.0) ||
+             (fabsf(angles[2] - ideal_xyz[2]) >= 10.0));
 }
 
 void run_application(void)
 {
-  int16_t raw_data[3];
-  float xyz_angles[3];
+    int16_t raw_data[3];
+    float xyz_angles[3];
 
-  while (1) {
-    LED_OFF();
-    _delay_ms(1000);
-    LED_ON();
+    while (1) {
+        LED_OFF();
+        _delay_ms(1000);
+        LED_ON();
 
-    memset(raw_data, 0, 3*sizeof(int16_t));
-    memset(xyz_angles, 0.0, 3*sizeof(float));
+        memset(raw_data, 0, sizeof(raw_data));
+        memset(xyz_angles, 0, sizeof(xyz_angles));
 
-    read_from_adxl345(raw_data);
-    get_xyz_angles(raw_data, xyz_angles);
+        read_from_adxl345(raw_data);
+        get_xyz_angles(raw_data, xyz_angles);
 
 #if (DEBUG == 1)
-    char logger_msg[255];
+        char logger_msg[255];
 
-    sprintf(logger_msg, "[LOG] (x/y/z): (%f/%f/%f) deg\n", xyz_angles[0], xyz_angles[1], xyz_angles[2]);
-    uart_logger(logger_msg);
+        sprintf(logger_msg, "[LOG] (x/y/z): (%f/%f/%f) deg\n", xyz_angles[0], xyz_angles[1], xyz_angles[2]);
+        uart_logger(logger_msg);
 #endif
 
     if (!posture_is_correct(xyz_angles)) {
-      uart_logger("[LOG] Incorrect posture detected...\n");
-      VIBRACALL_ON();
+        uart_logger("[LOG] Incorrect posture detected...\n");
+        VIBRACALL_ON();
     } else {
-      VIBRACALL_OFF();
+        VIBRACALL_OFF();
     }
   }
 }
 
 int main(void)
 {
-  sys_config();
-  uart_logger("[LOG] Starting the application...\n");
-  run_application();
+    sys_config();
+    uart_logger("[LOG] Starting the application...\n");
+    run_application();
 
-  return 1; // normally never reached...
+    return 1; // normally never reached...
 }
